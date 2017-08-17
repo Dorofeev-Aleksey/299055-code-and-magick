86e71fafae271618accb5f81ed7f0741c397b8ae
Dorofeev-Aleksey <mr.dorick@yandex.ru>
2017-08-18 00:25:21 +0300

    ЭEslint и ошибки в коде

--- js/stat.js
+++ js/stat.js
@@ -1,46 +1,42 @@
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
-  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'
+  ctx.fillRect(110, 20, 420, 270)
 
-  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)'; // white;
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)'
+  ctx.strokeRect(100, 10, 420, 270)
+  ctx.fillRect(100, 10, 420, 270)
 
-  ctx.fillStyle = '#000'; // black;
-  ctx.font = '16px PT Mono';
+  ctx.fillStyle = '#000' // black;
+  ctx.font = '16px PT Mono'
 
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+  ctx.fillText('Ура вы победили!', 120, 40)
+  ctx.fillText('Список результатов:', 120, 60)
 
-  var max = -1;
-  var maxIndex = -1;
-
-  for (var i = 0 ; i < times.length; i++) {
-    var time = times[i];
+  var max = -1
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i]
     if (time > max) {
-      max = time;
-      maxIndex = i;
+      max = time
     }
   }
 
-  var histogramHeight = 150;              // px;
-  var step = histogramHeight / max; // px;
+  var histogramHeight = 150 // px;
+  var step = histogramHeight / max // px;
 
-  var barWidth = 40; // px;
-  var indent = 50;    // px;
-  var initialX = 120; // px;
-  var initialY = 260;  // px;
-
-  ctx.textBaseline = 'top'; // положение надписи от левого верхнего угла
-  for(var i = 0; i < times.length; i++) {
-    var randomNumber = Math.random();
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  var barWidth = 40
+  var indent = 50
+  var initialX = 120
+  var initialY = 260
+  ctx.textBaseline = 'top' // положение надписи от левого верхнего угла
+  for (var n = 0; n < times.length; n++) {
+    var randomNumber = Math.random()
+    if (names[n] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)'
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, '+ randomNumber + ')';
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + randomNumber + ')'
     }
-    ctx.fillText(names[i], initialX + indent * i * 2, initialY);
-    ctx.fillRect(initialX + indent * i * 2 , initialY - times[i] * step - 5, barWidth, times[i] * step);
-    ctx.fillText(Math.round(times[i]), initialX + indent * i * 2, initialY - times[i] * step - 30);
+    ctx.fillText(names[n], initialX + indent * n * 2, initialY)
+    ctx.fillRect(initialX + indent * n * 2, initialY - times[n] * step - 5, barWidth, times[n] * step)
+    ctx.fillText(Math.round(times[n]), initialX + indent * n * 2, initialY - times[n] * step - 30)
   }
-};
+}
